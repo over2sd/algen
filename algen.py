ae130aa0281dcf80d29d1797364f9204294907eb
Lincoln Sayger <skrey_pub@cox.net>
2013-01-08 23:07:01 -0600

    Added type selection and new exercise forms.

--- algen.py
+++ algen.py
@@ -14,13 +14,14 @@ def main(argv):
   cnt = 1
   mn = 1
   mx = 10
-  vlist = "abcdefghiklmnopqrstuvwxyz"
+  vlist = "abcdefghikmnopqrstuvwxyz"
   twoside = False
   out = ""
   lines = []
   boring = [-1,0,1]
+  tlist = "01235"
   try:
-    opts, args = getopt.getopt(argv, "c:dhn:o:v:x:0", ["double-sided","count=","min=","max=","vars=","outfile=","help","allowzero"])
+    opts, args = getopt.getopt(argv, "c:dhn:o:v:x:0t:", ["double-sided","count=","min=","max=","vars=","outfile=","help","allowzero","types="])
   except getopt.error as err:
     print "Error: %s\n" % err
     usage()
@@ -40,6 +41,9 @@ def main(argv):
         mx = int(arg)
       else:
         print "Argument is not a valid number. Ignoring %s %s.\n" % (opt,arg)
+    elif opt in ("-t","--types"):
+      # need sanity check?
+      tlist = arg
     elif opt in ("-n","--min"):
       if isnum(arg):
         mn = int(arg)
@@ -61,22 +65,36 @@ def main(argv):
       except IOError as e:
         print "Could not open %s for output: %s. Skipping." % (out,e)
   if mx <= 1 and mn >=-1: boring = [] # only you can prevent infinite loops
-  for i in range(cnt):
+  exlist = []
+  while len(exlist) < cnt: # until we have at least as many listed types as we want exercises...
+    exlist += tlist # add on exercise types from list of desired types
+  exlist = exlist[:cnt] # trim to desired length
+  i = 0
+  for probtype in exlist:
     part1 = randint(mn,mx) # x
     part2 = randint(mn,mx) # a
     part3 = randint(mn,mx) # b
     part4 = randint(mn,mx) # c/y
+    part5 = randint(mn,mx) # d
     while part1 in boring: part1 = randint(mn,mx) # x
     while part2 in boring: part2 = randint(mn,mx) # a
     while part3 in boring: part3 = randint(mn,mx) # b
     while part4 in boring: part4 = randint(mn,mx) # c/y
+    while part5 in boring: part5 = randint(mn,mx) # d
     var = vlist[randint(0,len(vlist)-1)]
-    print "%i) " % (i+1),
-    probtype = '1'
+    i += 1
     o = ""
     if probtype == '0': o = showalgex.showaxpbeqc(part1,part2,var,part3)
     elif probtype == '1': o = showalgex.showaxbeqcxd(part1,part2,var,part3,part4)
-  if out is not "": lines.append("\n%s" % o)
+    elif probtype == '2': o = showalgex.showgcfax2pbxmc(part1,var,part2,part3,part4,part5)
+    elif probtype == '3':
+      if part1 < 1: part1 *= -1 # must be positive
+      if part2 < 1: part2 *= -1 # must be positive
+      if part1 == part2: part1 += 1 # must not be equal
+      o = showalgex.showtriangle(part1,part2)
+    elif probtype == '5': o = showalgex.showabxmc(part1,part2,part3,var,part4)
+    print "%i%s" % (i,o)
+    if out is not "": lines.append("\n%s" % o)
   if out is not "":
     with open(out,'a') as f:
       for l in lines:
@@ -106,16 +124,16 @@ def usage():
   print "-x <#>, --max:		Maximum value for variable, coefficients, and constants"
   print "-v <#>, --vars:		String of lowercase letters that can be used for variables (e.g., \"-v x\", \"-v abc\")"
   print "-o <file>, --outfile:		A filename where the program will append its results for easy copy/paste"
-#  print "-t <types>, --types:		Type(s) of problems to generate:"
-#  print "		0: ax+b=c"
-#  print "		1: ax+b=cx+d"
-#  print "		2: ax^2+bx-c (GCF)"
-#  print "		3: area/perim of a triangle"
+  print "-t <types>, --types:		Type(s) of problems to generate:"
+  print "		0: ax+b=c"
+  print "		1: ax+b=cx+d"
+  print "		2: ax^2+bx-c (GCF)"
+  print "		3: area/perim of a triangle"
 #  print "		4: area/perim of a quadrilateral"
-#  print "		5: a(bx-c)=d"
+  print "		5: a(bx-c)=d"
 #  print "		6: x^3-y^3"
 #  print "		7: ax^2+bx-c=0"
-#  print "	Examples: \"-t 138af\" \"-t 2\""
+  print "	Examples: \"-t 138af\" \"-t 2\""
   print "-0, --allowzero:		Allow value of x, a, b, c... to be boring (0, 1, or -1)"
 
 if __name__ == "__main__":
--- showalgex.py
+++ showalgex.py
@@ -3,7 +3,6 @@
 #  print "		0: ax+b=c"
 def showaxpbeqc(v,a,x,b):
   c = (v*a)+b
-  print "%i%c+%i=%i (%c=%i)" % (a,x,b,c,x,v)
   o = ") %i%c+%i=%i (%c=%i)" % (a,x,b,c,x,v)
   return o
 
@@ -12,19 +11,57 @@ def showaxbeqcxd(v,a,x,b,c):
   one = (v*a)+b
   d = one - (v*c)
   o = ""
-  if c < 0:
-    print "%i%c+%i=%i%i%c (%c=%i)" % (a,x,b,d,c,x,x,v)
+  if c < 0: #TODO: Clean this up so -- becomes +, etc.
     o = ") %i%c+%i=%i%i%c (%c=%i)" % (a,x,b,d,c,x,x,v)
   elif d < 0:
-    print "%i%c+%i=%i%c%i (%c=%i)" % (a,x,b,c,x,d,x,v)
     o = ") %i%c+%i=%i%c%i (%c=%i)" % (a,x,b,c,x,d,x,v)
   else:
-    print "%i%c+%i=%i%c+%i (%c=%i)" % (a,x,b,c,x,d,x,v)
     o = ") %i%c+%i=%i%c+%i (%c=%i)" % (a,x,b,c,x,d,x,v)
+  return o
 
 #  print "		2: ax^2+bx-c (GCF)"
+def showgcfax2pbxmc(v,x,d,e,f,g):
+  # for (dx+e)(fx+g)
+  h = d*f
+  i = d*g
+  j = e*f
+  k = e*g
+  a = h
+  b = i+j
+  c = k
+  t = (a*(v*v))+(b*v)-c
+  o = ") %i%c^2+%i%c-%i (%i%c+%i)(%i%c-%i) (f(%i)=%i)" % (a,x,b,x,c,d,x,e,f,x,g,v,t)
+  return o
+
 #  print "		3: area/perim of a triangle"
+def showtriangle(m,n):
+  if (n>m): # Shouldn't happen, but let's fix it automagically.
+    z = m
+    m = n
+    n = z
+  if (n==m): m += 1 # prevents a=0
+  a = (m*m)-(n*n)
+  b = 2*m*n
+  c = (m*m)+(n*n)
+  area = 0.5*(a*b)
+  p = a+b+c
+  h = ((a*a)+(b*b))/c
+#  print "Given %i and %i: a=%i b=%i c=%i=%i" % (m,n,a,b,c,h)
+  o = ") A right triangle has legs x cm and %icm and a hypotenuse of %icm. Find the area and perimeter. (x=%icm, a=%icm^2, p=%icm)" % (b,c,a,area,p)
+  return o
+
 #  print "		4: area/perim of a quadrilateral"
+
 #  print "		5: a(bx-c)=d"
+def showabxmc(v,a,b,x,c):
+  e = a*(b*v)
+  f = a*c
+  d = e-f
+  o = ") %i(%i%c-%i)=%i (%c=%i)" % (a,b,x,c,d,x,v)
+  return o
+
 #  print "		6: x^3-y^3"
+def showx3my3(v,x,a):
+  pass
+
 #  print "		7: ax^2+bx-c=0"
